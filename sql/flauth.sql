ec266681a53c0a72f1c9227d5ffb8baf2d2bb418
Claudio Prezzi <cprezzi@comsulting.ch>
2018-01-21 13:14:46 +0100

    Updated DB creation script

--- sql/flauth.sql
+++ sql/flauth.sql
@@ -2,7 +2,7 @@ CREATE DATABASE  IF NOT EXISTS `flauth` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `flauth`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: localhost    Database: flauth
+-- Host: 127.0.0.1    Database: flauth
 -- ------------------------------------------------------
 -- Server version	5.7.19-log
 
@@ -28,7 +28,7 @@ CREATE TABLE `events` (
   `eid` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   PRIMARY KEY (`eid`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `events` (
 
 LOCK TABLES `events` WRITE;
 /*!40000 ALTER TABLE `events` DISABLE KEYS */;
-INSERT INTO `events` VALUES (1,'Machine starting'),(2,'Machine start error'),(3,'Machine shutdown'),(4,'Tag login'),(5,'Tag logout');
+INSERT INTO `events` VALUES (1,'Machine started'),(2,'Machine start error'),(3,'Machine shutdown'),(4,'Tag login'),(5,'Tag logout'),(6,'Manual correction');
 /*!40000 ALTER TABLE `events` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -55,9 +55,9 @@ CREATE TABLE `logs` (
   `tid` int(11) DEFAULT NULL,
   `eid` int(11) NOT NULL,
   `remarks` text,
-  `invoiced` bit DEFAULT 0,
+  `iid` int(11) DEFAULT NULL,
   PRIMARY KEY (`lid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,7 +66,7 @@ CREATE TABLE `logs` (
 
 LOCK TABLES `logs` WRITE;
 /*!40000 ALTER TABLE `logs` DISABLE KEYS */;
-INSERT INTO `logs` VALUES (1,'2017-06-08 20:24:15',1,3,4,NULL),(2,'2017-06-08 21:15:00',1,3,5,NULL);
+INSERT INTO `logs` VALUES (1,'2017-06-08 20:24:15',3,1,4,NULL,NULL),(2,'2017-06-08 21:15:00',3,1,5,NULL,NULL),(3,'2018-01-06 12:35:44',1,0,1,'',NULL);
 /*!40000 ALTER TABLE `logs` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -80,7 +80,7 @@ DROP TABLE IF EXISTS `machines`;
 CREATE TABLE `machines` (
   `mid` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
-  `config` varchar(100) NOT NULL,
+  `config` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`mid`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -91,7 +91,7 @@ CREATE TABLE `machines` (
 
 LOCK TABLES `machines` WRITE;
 /*!40000 ALTER TABLE `machines` DISABLE KEYS */;
-INSERT INTO `machines` VALUES (1,'Lasersaur',''),(2,'Laser klein',''),(3,'CNC-Fräse',''),(4,'Drehbank gross',''),(5,'Drehbank klein',''),(6,'3d-Drucker Einstein',''),(7,'3d-Drucker Newton',''),(8,'3d-Drucker Hawking',''),(9,'3d-Drucker X400',''),(10,'3d-Drucker Solidoodle',''),(11,'3d-Drucker p3Steel','');
+INSERT INTO `machines` VALUES (1,'Lasersaur',''),(2,'Laser klein',NULL),(3,'CNC-Fräse',NULL),(4,'Drehbank gross',NULL),(5,'Drehbank klein',NULL),(6,'3d-Drucker Einstein',NULL),(7,'3d-Drucker Newton',NULL),(8,'3d-Drucker Hawking',NULL),(9,'3d-Drucker X400',NULL),(10,'3d-Drucker Solidoodle',NULL),(11,'3d-Drucker p3Steel',NULL);
 /*!40000 ALTER TABLE `machines` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -104,10 +104,10 @@ DROP TABLE IF EXISTS `rights`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `rights` (
   `rid` int(11) NOT NULL AUTO_INCREMENT,
-  `mid` int(11) NOT NULL,
   `tid` int(11) NOT NULL,
-  `start` timestamp DEFAULT NULL,
-  `end` timestamp DEFAULT NULL,
+  `mid` int(11) NOT NULL,
+  `start` date DEFAULT NULL,
+  `end` date DEFAULT NULL,
   PRIMARY KEY (`rid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -118,7 +118,7 @@ CREATE TABLE `rights` (
 
 LOCK TABLES `rights` WRITE;
 /*!40000 ALTER TABLE `rights` DISABLE KEYS */;
-INSERT INTO `rights` VALUES (1,1,1,'2018-01-01','2019-01-01'),(2,1,2,'2018-01-01','2019-01-01'),(3,1,3,'2018-01-01','2019-01-11'),(4,2,1,'2018-01-01','2019-01-01'),(5,2,2,'2018-01-01','2019-01-01');
+INSERT INTO `rights` VALUES (1,1,1,'2017-06-01','2018-01-01'),(2,1,2,'2017-06-01','2018-01-01'),(3,1,3,'2017-06-01','2018-01-11'),(4,2,1,'2017-06-20','2018-01-01'),(5,2,2,'2017-06-25','2017-07-01');
 /*!40000 ALTER TABLE `rights` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -147,6 +147,34 @@ LOCK TABLES `tags` WRITE;
 INSERT INTO `tags` VALUES (1,'0856898896','Claudio Prezzi'),(2,'0079830412','Admin');
 /*!40000 ALTER TABLE `tags` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password_hash` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'info@fablabwinti.ch','a57b647e8c19ff326a561c4bd31505d4','FabLab Admin',1),(2,'claudio.prezzi@fablabwinti.ch','949a6bf1501f74e475cc98fb651e05c6','Claudio Prezzi',2),(3,'andreas.bachmann@fablabwinti.ch','a57b647e8c19ff326a561c4bd31505d4','Andreas Bachmann',2),(4,'damian.schneider@fablabwinti.ch','a57b647e8c19ff326a561c4bd31505d4','Damian Schneider',2),(5,'patrick.gubelmann@fablabwinti.ch','a57b647e8c19ff326a561c4bd31505d4','Patrick Gubelmann',2);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -157,4 +185,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-08-12 14:41:17
+-- Dump completed on 2018-01-21 13:10:39
